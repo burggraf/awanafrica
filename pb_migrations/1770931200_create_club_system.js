3d71e58424f85cf65908e98677a65c1202b6ac99
Mark Burggraf <markb@mantisbible.com>
2026-02-12 08:56:48 -0800

    db: add core club management collections

---
+++ pb_migrations/1770931200_create_club_system.js
@@ -0,0 +1,50 @@
+migrate((app) => {
+  const clubs = new Collection({
+    name: "clubs",
+    type: "base",
+    fields: [
+      { name: "name", type: "text", required: true },
+      { name: "type", type: "select", values: ["church", "school", "other"], required: true },
+      { name: "address", type: "text" },
+      { name: "timezone", type: "text", defaultValue: "UTC" }
+    ],
+  });
+  app.save(clubs);
+
+  const memberships = new Collection({
+    name: "club_memberships",
+    type: "base",
+    fields: [
+      { name: "user", type: "relation", collectionId: "_pb_users_auth_", required: true, cascadeDelete: true },
+      { name: "club", type: "relation", collectionId: clubs.id, required: true, cascadeDelete: true },
+      { name: "roles", type: "select", values: ["Director", "Secretary", "Treasurer", "Leader"], maxSelect: 4, required: true }
+    ],
+    listRule: "user = @request.auth.id",
+    viewRule: "user = @request.auth.id",
+  });
+  app.save(memberships);
+
+  const programs = new Collection({
+    name: "programs",
+    type: "base",
+    fields: [
+      { name: "club", type: "relation", collectionId: clubs.id, required: true, cascadeDelete: true },
+      { name: "name", type: "text", required: true },
+      { name: "description", type: "text" }
+    ],
+    listRule: "@request.auth.id != '' && club.club_memberships_via_club.user.id ?= @request.auth.id",
+    viewRule: "@request.auth.id != '' && club.club_memberships_via_club.user.id ?= @request.auth.id",
+  });
+  app.save(programs);
+
+  // Now update clubs with rules
+  clubs.listRule = "@request.auth.id != '' && @collection.club_memberships.club.id ?= id && @collection.club_memberships.user.id ?= @request.auth.id";
+  clubs.viewRule = "@request.auth.id != '' && @collection.club_memberships.club.id ?= id && @collection.club_memberships.user.id ?= @request.auth.id";
+  app.save(clubs);
+}, (app) => {
+  const collections = ["programs", "club_memberships", "clubs"];
+  for (const name of collections) {
+    const collection = app.findCollectionByNameOrId(name);
+    if (collection) app.delete(collection);
+  }
+})
